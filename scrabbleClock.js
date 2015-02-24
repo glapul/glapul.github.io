8a30c55b182917b7c955c6d34fbc0e62cda54f01
Michał Glapa <michal.glapa@gmail.com>
2015-02-24 18:43:18 +0100

    beta

--- scrabbleClock.js
+++ scrabbleClock.js
@@ -1,7 +1,10 @@
 var num_of_players = 0;
 var current_player = 0;
-var INITIAL_TIME = "0:10:00"
+var INITIAL_TIME = 60 * 10;
+var time_remaining = [];
+var started = 0;
 
+var $ = document.getElementById;
 function f() {
     var player_input = document.getElementById("player_input");
     create_player(player_input.value);
@@ -23,7 +26,7 @@ function create_player(name) {
     time_paragraph.setAttribute("id", "time"+num_of_players);
 
     name_paragraph.appendChild(document.createTextNode(name));
-    time_paragraph.appendChild(document.createTextNode(INITIAL_TIME));
+    time_paragraph.appendChild(document.createTextNode(convert_to_time(INITIAL_TIME)));
 
     li.appendChild(name_paragraph);
     li.appendChild(time_paragraph);
@@ -31,6 +34,38 @@ function create_player(name) {
 
     ul.appendChild(li);
     //alert(li.id);
+    time_remaining[num_of_players] = INITIAL_TIME;
     num_of_players += 1
 }
+
+var tick = function () {
+    time_remaining[current_player] -= 1;
+    var time_paragraph = document.getElementById("time"+current_player);
+    time_paragraph.innerHTML = convert_to_time(time_remaining[current_player]);
+}
+
+function next() {
+    if(started == 0) {
+        document.getElementById("btn_text").innerHTML = "NEXT";
+        setInterval(tick, 1000);
+        started = 1;
+    }
+    else
+        current_player = (current_player + 1) % num_of_players;
+}
+
+function convert_to_time(seconds) {
+    if(seconds < 0)
+        return "0 : 00 : 00";
+    var s = seconds % 60;
+    seconds -= s;
+    seconds /= 60;
+    var m = seconds % 60;
+    seconds -= m;
+    seconds /= 60;
+    return "" + m + "m  " + s + "s";
+}
+
+
+
 
