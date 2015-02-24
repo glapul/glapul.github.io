5d5b737081f9266350734fa9e7473362bad25934
Michał Glapa <michal.glapa@gmail.com>
2015-02-24 19:35:48 +0100

    beta2

--- scrabbleClock.js
+++ scrabbleClock.js
@@ -1,13 +1,20 @@
 var num_of_players = 0;
 var current_player = 0;
-var INITIAL_TIME = 60 * 10;
+var INITIAL_TIME = -1;
 var time_remaining = [];
 var started = 0;
 
 var $ = document.getElementById;
 function f() {
     var player_input = document.getElementById("player_input");
-    create_player(player_input.value);
+    if(INITIAL_TIME == -1) {
+        INITIAL_TIME = 60 * parseInt(player_input.value);
+        player_input.placeholder = "Type to add players";
+        document.getElementById("next_btn").setAttribute("style", "visibility: visible;");
+    }
+    else {
+        create_player(player_input.value);
+    }
     player_input.value = "";
     return false;
 }
@@ -44,19 +51,44 @@ var tick = function () {
     time_paragraph.innerHTML = convert_to_time(time_remaining[current_player]);
 }
 
+var ticker;
+
 function next() {
     if(started == 0) {
         document.getElementById("btn_text").innerHTML = "NEXT";
-        setInterval(tick, 1000);
-        started = 1;
+        document.getElementById("pause_btn").setAttribute("style","visiblity : visible;");
+        ticker = setInterval(tick, 1000);
+        started = true;
+        document.getElementById("player_input").setAttribute("style", "visibility: hidden;");
     }
-    else
-        current_player = (current_player + 1) % num_of_players;
+    else {
+        var jumps = 0;
+        do {
+            current_player = (current_player + 1) % num_of_players;
+            jumps += 1;
+        } while(jumps < num_of_players  && time_remaining[current_player] < 0);
+        if(jumps == num_of_players) {
+            clearInterval(ticker);
+        }
+    }
+}
+var paused = false;
+function pause() {
+    if(!paused) {
+        paused = true;
+        clearInterval(ticker);
+        document.getElementById("pause_text").innerHTML = "CONTINUE";
+    }
+    else {
+        paused = false;
+        ticker = setInterval(tick, 1000);
+        document.getElementById("pause_text").innerHTML = "PAUSE";
+    }
 }
 
 function convert_to_time(seconds) {
     if(seconds < 0)
-        return "0 : 00 : 00";
+        return "time expired";
     var s = seconds % 60;
     seconds -= s;
     seconds /= 60;
